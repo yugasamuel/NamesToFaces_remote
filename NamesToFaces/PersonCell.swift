bfcef323af6a3fdeb2ac9c3a1c6a030c2095bae8
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-13 21:27:24 +0700

    Add UICollectionView data sources

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Yuga Samuel on 13/09/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
