8db86b32442136f5935c92a1f73f7f3f2794d631
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-13 20:10:02 +0700

    Initial Commit

---
+++ NamesToFaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Yuga Samuel on 13/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
