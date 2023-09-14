9b291e35b0965e02063d8006d874c67c3eb89fc2
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-14 18:41:33 +0700

    Change picker source type to camera

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } 
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
